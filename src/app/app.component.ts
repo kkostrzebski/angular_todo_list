3ddabc8954bf98f90628c7e064867543c6e844ae
Konrad <ko.kostrzebski@gmail.com>
2023-05-29 14:28:12 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { timeout } from 'rxjs';
+import { Task } from './task';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+
+
+  constructor() {
+
+  }
+
+ 
+}
+
+
